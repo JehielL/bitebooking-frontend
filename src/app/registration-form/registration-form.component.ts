23dff72d3824c0d083df413d4c028d6b4ed6e851
RONAL VELIZ <148784629+ronalveliz@users.noreply.github.com>
2024-03-04 16:59:56 +0100

    creacion de componente registration

---
+++ src/app/registration-form/registration-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './registration-form.component.html',
+  styleUrl: './registration-form.component.css'
+})
+export class RegistrationFormComponent {
+
+}
