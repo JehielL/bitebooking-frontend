cfcc0cdcfdcfa813540a5a0c460aab29c953619f
JehielL <jehiel.py@gmail.com>
2024-03-04 20:27:15 +0100

    cambio routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { BookingListComponent } from './booking-list/booking-list.component';
 import { BookingDetailComponent } from './booking-detail/booking-detail.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
-<<<<<<< HEAD
 import { LoginMainComponent } from './login-main/login-main.component';
-=======
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
->>>>>>> CarlosRama
 
 export const routes: Routes = [
    {
