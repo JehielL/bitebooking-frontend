705046205e42c898b545e6dba27a24bce4bf3203
JehielL <jehiel.py@gmail.com>
2024-03-04 16:56:06 +0100

    refactor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,19 @@
-import {Booking} from "./Interfaces/booking.model"
 import { Component } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { Menu } from "./Interfaces/menu.model";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
-<<<<<<< HEAD
-import { BookingDetailComponent } from "./booking-detail/booking-detail.component";
-import { BookingListComponent } from "./booking-list/booking-list.component";
-=======
-import { CarruselComponent } from './carrusel/carrusel.component';
+import { BookingDetailComponent } from './booking-detail/booking-detail.component';
+import { BookingListComponent } from './booking-list/booking-list.component';
 
->>>>>>> CarlosRama
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
   imports: [CommonModule, RouterOutlet, DatePipe, NavbarComponent, FooterComponent, BookingDetailComponent,NotFoundComponent, BookingListComponent],
-=======
-  imports: [CommonModule, RouterOutlet, DatePipe, NavbarComponent,
-     FooterComponent,NotFoundComponent,CarruselComponent],
->>>>>>> CarlosRama
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CarruselComponent } from '../carrusel/carrusel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarruselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
