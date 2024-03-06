338d5ca830f4f916375db97609fe6ddf5c2ec624
RONAL VELIZ <148784629+ronalveliz@users.noreply.github.com>
2024-03-06 01:13:18 +0100

    from registro
    creación de formulario de registro

---
+++ src/app/Interfaces/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: number,
+    firtsName: string,
+    lastName: string,
+    birthdayDate: Date,
+    email: string,
+    password: string,
+    phone: number,
+    //role:[]
+}
