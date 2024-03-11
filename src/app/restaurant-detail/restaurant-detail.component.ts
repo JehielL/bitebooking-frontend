6d774507655c2643b0a2a87ac8ac0f68932a71ab
CarlosDavidFelizRamirez <carlos.felizramirez.3@gmail.com>
2024-03-11 16:05:51 +0100

    guardar

---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-restaurant-detail',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './restaurant-detail.component.html',
+  styleUrl: './restaurant-detail.component.css'
+})
+export class RestaurantDetailComponent {
+restaurant: any;
+}
