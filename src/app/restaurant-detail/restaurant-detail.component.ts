d69ee1220f5d6783f4b3011216c0bec8cdd8bdd9
CarlosDavidFelizRamirez <carlos.felizramirez.3@gmail.com>
2024-03-19 17:53:11 +0100

    perfect!

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {Restaurant} from "../Interfaces/restaurant.model";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-restaurant-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './restaurant-detail.component.html',
   styleUrl: './restaurant-detail.component.css'
 })
 export class RestaurantDetailComponent {
+  restaurant: Restaurant [] = [];
 }
