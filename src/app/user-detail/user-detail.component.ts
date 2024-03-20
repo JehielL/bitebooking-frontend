8707630468614623f48744b73c6f123cdfe6596d
johnsebstian <tatamarcas273@gmail.com>
2024-03-20 13:33:45 -0800

    actualizao el user-detail (.ts)

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { User } from '../Interfaces/user.model';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, HttpClientModule, ReactiveFormsModule, NgbAlertModule  ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.css'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+
+  users: User | undefined;
+
+  images: string[] = [
+
+  ]
+  
+  constructor(
+    private httpClient: HttpClient,
+    private activatedRoute: ActivatedRoute){}
+
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      const id = params['id'];
+
+      if (!id) return;
+
+
+    const url = 'http://localhost:8080/user/' + id;
+
+    this.httpClient.get<User>(url).subscribe(b => this.users = b);
+    })
+  }
+
+  
 
 }
