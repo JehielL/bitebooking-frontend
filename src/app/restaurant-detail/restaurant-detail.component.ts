203b269b2663b73d2010e521693c6ee9bafc11d8
CarlosDavidFelizRamirez <carlos.felizramirez.3@gmail.com>
2024-03-20 10:46:13 +0100

    Vinculado correctamente

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { Restaurant } from '../Interfaces/restaurant.model';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { RouterLink } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, RouterLink} from "@angular/router";
+import {Restaurant} from "../Interfaces/restaurant.model";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Booking} from "../Interfaces/booking.model";
 
 @Component({
   selector: 'app-restaurant-detail',
@@ -10,5 +11,23 @@ import { RouterLink } from '@angular/router';
   templateUrl: './restaurant-detail.component.html',
   styleUrl: './restaurant-detail.component.css'
 })
-export class RestaurantDetailComponent {
+export class RestaurantDetailComponent implements OnInit{
+  restaurant: Restaurant | undefined;
+  constructor(
+      private activatedRoute: ActivatedRoute,
+      private httpClient: HttpClient
+  ) {}
+  ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe(params => {
+      const id = params['id'];
+
+      if (!id) return;
+
+      const url = 'http://localhost:8080/restaurant/' + id;
+
+      this.httpClient.get<Restaurant>(url).subscribe(restaurant => this.restaurant = restaurant);
+
+    });
+}
 }
