ef2778ebf948e7cf0ce5c14e83523284b7e7645c
Carlos David feliz Ramírez <95054371+CarlosDavidFelizRamirez@users.noreply.github.com>
2024-03-30 12:23:00 +0100

    preparativos finales en restaurant detail! a falta de confirmar.

--- src/app/Interfaces/restaurant.model.ts
+++ src/app/Interfaces/restaurant.model.ts
@@ -13,8 +13,6 @@ export interface Restaurant {
     closingTime: Date;
     averageRating: number;
     imageUrl: string;
-    //bookings: Booking;
-    //tables: Tables[];
     status: boolean;
   }
 
