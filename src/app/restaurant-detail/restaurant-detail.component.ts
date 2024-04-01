58ed83c506156382707345f87e5a986098eba38f
Carlos David feliz Ramírez <95054371+CarlosDavidFelizRamirez@users.noreply.github.com>
2024-04-01 19:01:47 +0200

    Arreglos para el buscador del restaurant list y ultimos cambios de restaurant detail

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,33 +1,38 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, RouterLink} from "@angular/router";
-import {Restaurant} from "../Interfaces/restaurant.model";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {Booking} from "../Interfaces/booking.model";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { Restaurant } from "../Interfaces/restaurant.model";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-restaurant-detail',
   standalone: true,
-  imports: [RouterLink,HttpClientModule],
+  imports: [RouterLink, HttpClientModule, DatePipe],
   templateUrl: './restaurant-detail.component.html',
-  styleUrl: './restaurant-detail.component.css'
+  styleUrls: ['./restaurant-detail.component.css']
 })
-export class RestaurantDetailComponent implements OnInit{
+export class RestaurantDetailComponent implements OnInit {
   restaurant: Restaurant | undefined;
-  constructor(
-      private activatedRoute: ActivatedRoute,
-      private httpClient: HttpClient
-  ) {}
-  ngOnInit(): void {
+  openingTime: Date | undefined;
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpClient: HttpClient
+  ) {}
+
+  ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
       const id = params['id'];
-
       if (!id) return;
 
-      const url = 'http://localhost:8080/restaurant/' + id;
+      const url = `http://localhost:8080/restaurant/${id}`;
+      this.httpClient.get<Restaurant>(url).subscribe(restaurant => {
 
-      this.httpClient.get<Restaurant>(url).subscribe(restaurant => this.restaurant = restaurant);
+    
 
+        console.log(restaurant);
+        this.restaurant = restaurant;
+      });
     });
-}
+  }
 }
