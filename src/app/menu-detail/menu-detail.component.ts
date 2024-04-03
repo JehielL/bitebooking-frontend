5acc3422ce52e5db76b836d90a28ee10708dce61
JehielL <jehiel.py@gmail.com>
2024-04-03 02:30:15 +0200

    Agregado rating.

--- src/app/menu-detail/menu-detail.component.ts
+++ src/app/menu-detail/menu-detail.component.ts
@@ -2,62 +2,64 @@ import { DatePipe } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { NgbAccordionModule, NgbAlert, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAccordionModule, NgbAlert, NgbAlertModule, NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 import { Menu } from '../Interfaces/menu.model';
 import { Dish } from '../Interfaces/dish.model';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Rating } from '../Interfaces/rating.model';
 
 
 @Component({
   selector: 'app-menu-detail',
   standalone: true,
-  imports: [HttpClientModule, RouterLink,DatePipe, NgbAccordionModule, NgbAlert],
+  imports: [HttpClientModule, RouterLink, DatePipe, NgbAccordionModule, NgbAlert, NgbRatingModule, ReactiveFormsModule],
   templateUrl: './menu-detail.component.html',
   styleUrl: './menu-detail.component.css'
 })
-export class MenuDetailComponent implements OnInit{
+export class MenuDetailComponent {
 
   menu: Menu | undefined;
-  dishes: Dish[] = [];                               
-  photoFile: File | undefined;
-  photoPreview: string | undefined;
-  isUpdate: boolean = false;
 
-  
+
+  ratings: Rating[] = [];
+  ratingForm = new FormGroup({
+
+    score: new FormControl(0),
+    comment: new FormControl(''),
+  });
+
+
+
   showDeleteMenuMessage: boolean = false;
+dishes: any;
 
   constructor(
 
     private activedRoute: ActivatedRoute,
-    private httpClient: HttpClient,
-    private router: Router
-  ) { 
+    private httpClient: HttpClient
+  ) {
 
-    
+
   }
-  
+
   ngOnInit(): void {
 
-   
-    
     this.activedRoute.params.subscribe(params => {
-      
       const id = params['id'];
       if (!id) return;
       const url = 'http://localhost:8080/menus/' + id;
       this.httpClient.get<Menu>(url).subscribe(m => this.menu = m);
 
-      this.httpClient.get<Dish[]>('http://localhost:8080/dishes/filter-by-menu/' + id)
-      .subscribe(dishes => this.dishes = dishes);
-
-      
+      this.httpClient.get<Rating[]>('http://localhost:8080/menus/filter-by-menu/' + id)
+        .subscribe(ratings => this.ratings = ratings);
     });
- 
-   
+
+
+
   }
 
-  delete(menu: Menu) {
-    const url = 'http://localhost:8080/menus/' + menu.id;
+  delete(menus: Menu) {
+    const url = 'http://localhost:8080/menus/' + menus.id;
     this.httpClient.delete(url).subscribe(response => {
       this.menu = undefined;
       this.showDeleteMenuMessage = true;
@@ -65,11 +67,32 @@ export class MenuDetailComponent implements OnInit{
   }
 
 
+
   hideDeletedMenuMessage() {
     this.showDeleteMenuMessage = false;
   }
-  
 
-  
+
+
+
+
+  save() {
+
+    const rating: Rating = {
+      id: 0,
+      score: this.ratingForm.get('score')?.value ?? 0,
+      comment: this.ratingForm.get('comment')?.value ?? '',
+      menu: this.menu,
+
+    };
+
+    this.httpClient.post<Rating>('http://localhost:8080/ratings', rating).subscribe(rating => {
+      this.ratingForm.reset();
+    });
+
+    this.httpClient.get<Rating[]>('http://localhost:8080/menus/filter-by-menu/' + this.menu?.id)
+    .subscribe(ratings => this.ratings = ratings);
+  }
+
 
 }
