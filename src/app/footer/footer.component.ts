730abb6e19604868df32b1bd6b5c3e8f4140620e
JehielL <jehiel.py@gmail.com>
2024-04-05 01:45:07 +0200

    refactor footer, navbar, ajustados permisos de visualizacion menu-detail.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 @Component({
   selector: 'app-footer',
@@ -9,5 +10,21 @@ import { RouterLink } from '@angular/router';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  isAdmin = false;
+  title = 'frontend'
+  isLoggedin = false;
+  collapsed = true;
+  userEmail = '';
+ 
+
+  constructor(private authService: AuthenticationService,
+    private router: Router){
+
+    this.authService.isLoggedin.subscribe(isLoggedin => this.isLoggedin = isLoggedin);
+    this.authService.userEmail.subscribe(userEmail => this.userEmail = userEmail);
+    this.authService.isAdmin.subscribe(isAdmin => this.isAdmin = isAdmin);
+  }
+
 
 }
