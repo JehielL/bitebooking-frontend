35f5cca9c835af2d2873a738c883dc2d67bc9542
johnsebstian <tatamarcas273@gmail.com>
2024-04-05 15:35:11 -1200

    esta intencion conflictos

--- src/app/dashboard-user/dashboard-user.component.ts
+++ src/app/dashboard-user/dashboard-user.component.ts
@@ -7,77 +7,61 @@ import { Role, User } from '../Interfaces/user.model';
 @Component({
   selector: 'app-dashboard-user',
   standalone: true,
-  imports: [RouterLink,ReactiveFormsModule, HttpClientModule,RouterLink],
+  imports: [RouterLink,ReactiveFormsModule, HttpClientModule, RouterLink],
   templateUrl: './dashboard-user.component.html',
   styleUrl: './dashboard-user.component.css'
 })
 
-export class DashboardUserComponent  implements OnInit {
-  isUpdate: boolean = true; // por defecto estamos en CREAR no en ACTUALIZAR 
-  users: User[] = [];
+export class DashboardUserComponent implements OnInit { 
+  users: User | undefined;
+  images: string[] = []
+  registerUserForm: any;
   photoFile: File | undefined;
   photoPreview: string | undefined;
-  roles = Role; // Esto hará que los valores de la enum estén disponibles en el HTML
+  router: any;
+  
+  constructor(
+    private httpClient: HttpClient,
+    private activatedRoute: ActivatedRoute){}
 
-  registerUserForm = new FormGroup({
-    id: new FormControl (0),
-    firtsName: new FormControl('',Validators.required),
-    lastName: new FormControl('',Validators.required),
-    birthdayDate: new FormControl(new Date()),
-    email: new FormControl('',[Validators.required, Validators.email]),
-    password: new FormControl('',[Validators.required, Validators.minLength(8),Validators.maxLength(30)]),
-    passwordConfirm: new FormControl('',[Validators.required, Validators.minLength(8),Validators.maxLength(30)]),
-    phone: new FormControl('',[Validators.required, Validators.pattern('^[0-9]{9}$')]),
-    role: new FormControl<Role>(Role.USER)
-  },
-  {validators: this.passwordConfirmValidator}
-  );
-  backendUser: any;
-user: any;
 
-  constructor(private httpClient : HttpClient,
-              private router: Router,  // esto es para navegar
-              private activatedRoute: ActivatedRoute,
-              private fb: FormBuilder
-  ){}
- 
-  passwordConfirmValidator(control: AbstractControl){
-
-    if(control.get('password')?.value === control.get('passwordConfirm')?.value){
-      return null;
-
-    }else{
-        return{
-          'confirmError': true
-        }
-    }
-  } 
-
-  ngOnInit(): void { 
-    this.activatedRoute.params.subscribe(params => {
-      const id = params['id'];
-      if(!id) return;
-
-      this.httpClient.get<User>('http://localhost:8080/user/' + id).subscribe(backendUser => {
-        
-        this.registerUserForm.reset({
-          id: backendUser.id,
-          firtsName:backendUser.firtsName,
-          lastName:backendUser.lastName,
-          birthdayDate:backendUser.birthdayDate,
-          email: backendUser.email,
-          password: backendUser.password,
-          phone: backendUser.phone,
-          role: backendUser.role,
-        });
-           // marcar boolean true isUpdate para utilizar en  mismo form
-        this.isUpdate = true; 
-
-        
-
-      });
+  ngOnInit(): void {
+    AOS.init();
+    this.httpClient.get<User[]>('http://localhost:8080/user').subscribe(userbacken => {
+      console.log(userbacken);
     });
+
+    this.activatedRoute.params.subscribe(params =>{
+      const id = params['id'];
+      if (!id) return;
+      this.httpClient.get<User>('http://localhost:8080/user') + id).suscribe(userbacken => {
+      this.registerUserForm.reset({
+        id: userbacken.id,
+        firstname: userbacken.firstname,
+        lastname: userbacken.lastname,
+        email: userbacken.email,
+        phone: userbacken.phone,
+        imgUser: userbacken.imgUser
+      });
+
+      });  
   }
+
+  
+
+
+  save(){
+    const user: User = this.registerUserForm.value as unknown as User;
+    console.log(user)
+
+    
+      const url = 'http://localhost:8080/user/'+ user.id;
+      this.httpClient.put<User>(url,user).subscribe(backendUser =>{
+        this.router.navigate(['/user',backendUser.id,'detail']);
+      });
+    
+  }
+
   onFileChange(event: Event) {
     let target = event.target as HTMLInputElement; // este target es el input de tipo file donde se carga el archivo
 
@@ -92,28 +76,7 @@ user: any;
     reader.onload = event => this.photoPreview = reader.result as string;
     reader.readAsDataURL(this.photoFile);
   }
-  save(){
-    const user: User = this.registerUserForm.value as User;
-    console.log(user)
 
-    if(this.isUpdate){
-      const url = 'http://localhost:8080/user/'+ user.id;
-      this.httpClient.put<User>(url,user).subscribe(backendUser =>{
-        this.router.navigate(['/user',backendUser.id,'detail']);
-      });
-    }else{
-      const url = 'http://localhost:8080/user';
-      this.httpClient.post<User>(url,user).subscribe(backendUser =>{
-        this.router.navigate(['/user',backendUser.id,'detail']); 
-      });
-    }
-  }
-  compareObjects(o1: any, o2: any): boolean {
-    // console.log("Comparando objetos: ", o1, o2);
 
-    if(o1 && o2) {
-      return o1.id === o2.id;
-    }
-    return o1 === o2;
-  } 
 }
+
