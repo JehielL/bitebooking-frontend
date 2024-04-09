3a6c5f4815a6062dfc46bcbbf212477c1588fe57
Carlos David feliz Ramírez <95054371+CarlosDavidFelizRamirez@users.noreply.github.com>
2024-04-09 16:33:30 +0200

    añadida la protecciones de direccion en el navegador! y se ha actualizado el componente cocina para que sea mas optimo

--- src/app/kitchen/kitchen.component.ts
+++ src/app/kitchen/kitchen.component.ts
@@ -1,69 +1,44 @@
 import { Component } from '@angular/core';
-import { RestaurantType } from '../Interfaces/restaurantType.model';
 import { Router } from '@angular/router';
-import { NgFor, KeyValuePipe } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { RestaurantType } from '../Interfaces/restaurantType.model';
 
 @Component({
   selector: 'app-kitchen',
   templateUrl: './kitchen.component.html',
   styleUrls: ['./kitchen.component.css'],
   standalone: true,
-  imports: [NgFor, KeyValuePipe]
+  imports: [NgFor]
 })
 export class KitchenComponent {
-  private RESTAURANT_TYPE_MAPPING: { [key: string]: string } = {
-    Japanese: 'JAPANESE_FOOD',
-    Thai: 'THAI_FOOD',
-    Spanish: 'SPAIN_FOOD',
-    Chinese: 'CHINESE_FOOD',
-    Brunch: 'BRUNCH',
-    'Coffee Store': 'COFFEE_STORE',
-    Bar: 'BAR',
-    Vietnamese: 'VIETNAM_FOOD',
-    Italian: 'ITALIAN_FOOD',
-    French: 'FRENCH_FOOD',
-    'Tex-Mex': 'TEX_MEX_FOOD',
-    Korean: 'KOREAN_FOOD',
-    Vegan: 'VEGAN_FOOD',
-    American: 'AMERICAN_FOOD',
-    German: 'GERMAN_FOOD',
-    Portuguese: 'PORTUGUESE_FOOD',
-    Fusion: 'FUSION_FOOD',
-    Greek: 'GREEK_FOOD',
-    Indian: 'INDIAN_FOOD',
-    Peruvian: 'PERUVIAN_FOOD',
-    Canadian: 'CANADIAN_FOOD',
-    Dominican: 'DOMINICAN_FOOD',
-    'Latin American': 'LATIN_AMERICAN_FOOD',
-    Argentine: 'ARGENTINE_FOOD',
-    Balkan: 'BALKAN_FOOD',
-    Georgian: 'GEORGIAN_FOOD',
-    Arabian: 'ARABIAN_FOOD',
-    Marrakech: 'MARRAKECH_FOOD',
-    Asian: 'ASIAN_FOOD',
-    African: 'AFRICAN_FOOD',
-    Maghreb: 'MAGHREB_FOOD',
-    Caribbean: 'CARIBBEAN_FOOD'
-  };
+  cocinasPorLetra: { [letra: string]: string[] } = {};
+  objectKeys = Object.keys;
 
-  cocinas = Object.values(RestaurantType).map(nombre => ({ letra: nombre[0], nombre }));
-
-  cocinasPorLetra = this.cocinas.reduce((acc: { [key: string]: { letra: string; nombre: string }[] }, cocina) => {
-    if (!acc[cocina.letra]) {
-      acc[cocina.letra] = [];
-    }
-    acc[cocina.letra].push(cocina);
-    return acc;
-  }, {});
-
-  constructor(private router: Router) {}
-
-  convertirANombreEnum(nombreAmigable: string): string {
-    return this.RESTAURANT_TYPE_MAPPING[nombreAmigable] || nombreAmigable;
+  constructor(private router: Router) {
+    this.prepararCocinasPorLetra();
   }
 
-  navegarATipoCocina(tipoCocina: string) {
-    const tipoCocinaBackend = this.convertirANombreEnum(tipoCocina);
-    this.router.navigate(['/restaurant-list', tipoCocinaBackend]);
+  prepararCocinasPorLetra() {
+    
+    const tiposCocina = Object.values(RestaurantType);
+    
+    tiposCocina.forEach((nombreCocina) => {
+      const letra = nombreCocina[0].toUpperCase();
+      if (!this.cocinasPorLetra[letra]) {
+        this.cocinasPorLetra[letra] = [];
+      }
+      this.cocinasPorLetra[letra].push(nombreCocina);
+    });
+
+    for (const letra in this.cocinasPorLetra) {
+      this.cocinasPorLetra[letra].sort();
+    }
+  }
+
+  navegarATipoCocina(nombreCocina: string) {
+    const tipoCocinaBackend = Object.keys(RestaurantType).find(key => RestaurantType[key as keyof typeof RestaurantType] === nombreCocina);
+    if (tipoCocinaBackend) {
+      this.router.navigate(['/restaurant-list', tipoCocinaBackend]);
+    }
   }
 }
